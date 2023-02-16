deb5cd7185790e7ed3e68edec5a44bbc684a450c
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-02-16 11:16:25 +0530

    Added Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace LineComparisonComputation
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Line Comparison Computation Program");
+
+        }
+    }
+}
+
